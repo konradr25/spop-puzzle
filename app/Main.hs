0661618fe8b59dbc3e4d0708b79f4bbfd4489123
dratajczak <ratajczakdamian@gmail.com>
2019-01-24 01:50:39 +0100

    Main Fix

--- app/Main.hs
+++ app/Main.hs
@@ -6,13 +6,22 @@ import System.IO
 
 main :: IO ()
 main = do
+    -- Get the file name of board
+    putStr "What is the board definition filename? "
+    hFlush stdout
+    fileNameOfBoard <- getLine
 
     -- Getting file content
-    boardFileContent <- readFile "hor_board.txt"
+    boardFileContent <- readFile fileNameOfBoard
     let boardLines = lines boardFileContent
 
+    -- Get the file name of words
+    putStr "What is the filename of words? "
+    hFlush stdout
+    fileNameOfWords <- getLine
+
     -- Getting file content
-    wordsFileContent <- readFile "hor_words.txt"
+    wordsFileContent <- readFile fileNameOfWords
     let wordsLines = lines wordsFileContent
 
     -- finding secret word
